093423d8ec1d06fc7ce8fa28602b119c908e876d
Tunmisesuliat <tunmisesuliat@gmail.com>
2020-07-31 22:22:34 +0100

    updates

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 5005;
+app.use(favicon(path.join(__dirname, '../build', 'favicon.ico')));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(path.join(__dirname, '../build')));
+app.get('/p', function (req, res) {
+  return res.send('Welcome to Gidara');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+app.listen(port, () => console.log(`app is running on port ${port}`));
